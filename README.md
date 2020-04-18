bcaa4e1ee06387a55f7e2a9b44c39309be04f29a
Ardalan Naghshineh <ardalan.naghshineh@gmail.com>
2020-04-17 22:20:19 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Persian Words Game Solver
+
+## Word Databases
+- https://raw.githubusercontent.com/reza1615/Persian-Spell-checker/master/Word_collection_1
+- https://bigdata-ir.com/%D9%81%D8%A7%DB%8C%D9%84-%D8%AF%DB%8C%DA%A9%D8%B4%D9%86%D8%B1%DB%8C-%D8%A8%D8%A7-%D9%81%D8%B1%D9%85%D8%AA-csv/
---
+++ docs/script.js
@@ -0,0 +1,48 @@
+const input = document.getElementById("letters");
+const resultDiv = document.getElementById("result");
+
+input.addEventListener("keyup", (e) => {
+  solve(e.target.value.split(""));
+});
+
+const solve = async (letters, minLength = 3, maxLength = letters.length) => {
+  let words = [...(await wordsPromise)];
+
+  words = words.filter((word) => {
+    if (word)
+      return !word.split("").find((letter) => !letters.includes(letter));
+  });
+
+  // console.log('remove duplicates...')
+  // words = words.filter((word,index) => words.findIndex(w => w === word) === index);
+
+  console.log("sort...");
+  words.sort((a, b) => b.length - a.length);
+
+  console.log("remove long or short words...");
+  words = words.filter(
+    (word) => word.length <= maxLength && word.length >= minLength
+  );
+
+  console.log("print...");
+  let resultHTML = "";
+  words.forEach((word) => {
+    resultHTML += `<li>${word}</li>`;
+  });
+  resultDiv.innerHTML += "<ul>" + resultHTML + "</ul>";
+};
+
+const wordsPromise = new Promise((resolve) => {
+  fetch("./databases/farsi-db.csv").then(async (collection1) => {
+    const words1 = (await collection1.text())
+      .split("\n")
+      .map((line) => line.split(",")[1]);
+
+    return fetch("./databases/farsi-Word_collection_1.txt").then(
+      async (collection2) => {
+        const words2 = (await collection2.text()).split("\n");
+        resolve([...words1, ...words2]);
+      }
+    );
+  });
+});
